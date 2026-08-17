98815043789a22fbc3767e6396d966a73a6e2f55
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-08-17 18:25:37 +0600

    fix the dns issues

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const bcrypt = require("bcryptjs");
